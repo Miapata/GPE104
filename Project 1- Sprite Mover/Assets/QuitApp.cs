8db2d3a1d7fa80a2925c426f84aa5f5d71a81622
KNOWN-UNIVERSE\miapata <miapata@uat.edu>
2018-05-23 09:17:52 -0700

    no message

---
+++ Project 1- Sprite Mover/Assets/QuitApp.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitApp : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+
+		//If escape key is pressed
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			Application.Quit();
+		}
+	}
+}
