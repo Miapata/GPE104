4f568739eccfae388627203b23d043c32ed85f16
MIAPATA <MIAPATA@uat.edu>
2018-06-06 13:24:21 -0700

    no message

--- Project2 - Milestone 2/Assets/Explosion.cs
+++ Project2 - Milestone 2/Assets/Explosion.cs
@@ -12,19 +12,38 @@ public class Explosion : MonoBehaviour {
 	}
 
 	void Update(){
+        //If the ring animation is finished
 		if (anim.GetCurrentAnimatorStateInfo (0).normalizedTime >= 1) {
-			
+			//Spanw missle
 			GameObject newMissle= Instantiate(missle,transform.position,Quaternion.identity);
-		
+		    //Multiply the worldspace by the quaternion    Vector right *rotation
+            //Declare rigidbody2d
 			Rigidbody2D rigidBody = newMissle.GetComponent<Rigidbody2D> ();
+
+            //set z transform
 			newMissle.transform.position = new Vector3 (newMissle.transform.position.x, newMissle.transform.position.y, 0);
+
+            //get a vector direction
 			var dir = player.transform.position - newMissle.transform.position;
+
+            //radians
 			var radian = Mathf.Atan2 (dir.y, dir.x);
+            //Covert radians to degrees
 			float degress = radian * Mathf.Rad2Deg;
+
+            //Set the missle rotation
 			newMissle.transform.rotation = Quaternion.Euler(0,0,degress-90);
+
+            //dirction of velocity
 			Vector2 direction = (player.transform.position - newMissle.transform.position);//W
+
+            //normalize it
 			var vel = direction.normalized;
-			rigidBody.velocity = vel*2;
+
+
+			rigidBody.velocity = vel*4;
+            
+            
 		}
 	}
 }
