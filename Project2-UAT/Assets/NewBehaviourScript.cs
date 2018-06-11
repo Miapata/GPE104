378a6d7c09af22f8e63770f7e35f564006bba72b
KNOWN-UNIVERSE\miapata <miapata@uat.edu>
2018-06-11 13:28:25 -0700

    Commit

---
+++ Project2-UAT/Assets/NewBehaviourScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    Rigidbody2D rb;
+    public float rotateSpeed, movementSpeed;
+	// Use this for initialization
+	void Start() { 
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow)){
+            transform.Rotate(0, 0,rotateSpeed*Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, 0, -rotateSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(0, movementSpeed * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(0, -movementSpeed * Time.deltaTime, 0);
+        }
+
+    }
+}
